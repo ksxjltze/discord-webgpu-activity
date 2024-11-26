e2c50e2528e25cc28b932511e2fbf9b4ec73d1b8
Lee Jia Keat <leejiakeat@gmail.com>
2024-11-26 19:56:03 +0800

    first commit

---
+++ client/main.js
@@ -0,0 +1,222 @@
+import { DiscordSDK } from "@discord/embedded-app-sdk";
+
+import rocketLogo from '/rocket.png';
+import "./style.css";
+
+// Will eventually store the authenticated user's access_token
+let auth;
+
+const discordSdk = new DiscordSDK(import.meta.env.VITE_DISCORD_CLIENT_ID);
+setupDiscordSdk().then(() => {
+  console.log("Discord SDK is authenticated");
+
+  // We can now make API calls within the scopes we requested in setupDiscordSDK()
+  // Note: the access_token returned is a sensitive secret and should be treated as such
+  appendVoiceChannelName();
+  appendGuildAvatar();
+  initRenderer();
+});
+
+async function setupDiscordSdk() {
+  await discordSdk.ready();
+  console.log("Discord SDK is ready");
+
+  // Authorize with Discord Client
+  const { code } = await discordSdk.commands.authorize({
+    client_id: import.meta.env.VITE_DISCORD_CLIENT_ID,
+    response_type: "code",
+    state: "",
+    prompt: "none",
+    scope: [
+      "identify",
+      "guilds",
+      "applications.commands"
+    ],
+  });
+
+  // Retrieve an access_token from your activity's server
+  // Note: We need to prefix our backend `/api/token` route with `/.proxy` to stay compliant with the CSP.
+  // Read more about constructing a full URL and using external resources at
+  // https://discord.com/developers/docs/activities/development-guides#construct-a-full-url
+  const response = await fetch("/.proxy/api/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      code,
+    }),
+  });
+  const { access_token } = await response.json();
+
+  // Authenticate with Discord client (using the access_token)
+  auth = await discordSdk.commands.authenticate({
+    access_token,
+  });
+
+  if (auth == null) {
+    throw new Error("Authenticate command failed");
+  }
+}
+
+async function initRenderer() {
+  if (!navigator.gpu) {
+    throw new Error("WebGPU not supported on this browser.");
+  }
+
+  const canvas = document.querySelector("canvas");
+
+  const adapter = await navigator.gpu.requestAdapter();
+  if (!adapter) {
+    throw new Error("No appropriate GPUAdapter found.");
+  }
+
+  const device = await adapter.requestDevice();
+
+  const context = canvas.getContext("webgpu");
+  const canvasFormat = navigator.gpu.getPreferredCanvasFormat();
+  context.configure({
+    device: device,
+    format: canvasFormat,
+  });
+
+  //draw triangle
+  const vertices = new Float32Array([
+    //   X,    Y,
+    -0.8, -0.8, // Triangle 1 (Blue)
+    0.8, -0.8,
+    0.8, 0.8,
+
+    -0.8, -0.8, // Triangle 2 (Red)
+    0.8, 0.8,
+    -0.8, 0.8,
+  ]);
+
+  const vertexBuffer = device.createBuffer({
+    label: "Cell vertices",
+    size: vertices.byteLength,
+    usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
+  });
+
+  device.queue.writeBuffer(vertexBuffer, /*bufferOffset=*/0, vertices);
+
+  const vertexBufferLayout = {
+    arrayStride: 8,
+    attributes: [{
+      format: "float32x2",
+      offset: 0,
+      shaderLocation: 0, // Position, see vertex shader
+    }],
+  };
+
+  const cellShaderModule = device.createShaderModule({
+    label: "Cell shader",
+    code: `
+        @vertex
+        fn vertexMain(@location(0) pos: vec2f) ->
+          @builtin(position) vec4f {
+          return vec4f(pos, 0, 1);
+        }
+
+        @fragment
+        fn fragmentMain() -> @location(0) vec4f {
+          return vec4f(1, 0, 0, 1); // (Red, Green, Blue, Alpha)
+        }
+      `
+  });
+
+  const cellPipeline = device.createRenderPipeline({
+    label: "Cell pipeline",
+    layout: "auto",
+    vertex: {
+      module: cellShaderModule,
+      entryPoint: "vertexMain",
+      buffers: [vertexBufferLayout]
+    },
+    fragment: {
+      module: cellShaderModule,
+      entryPoint: "fragmentMain",
+      targets: [{
+        format: canvasFormat
+      }]
+    }
+  });
+
+  const encoder = device.createCommandEncoder();
+  const pass = encoder.beginRenderPass({
+    colorAttachments: [{
+      view: context.getCurrentTexture().createView(),
+      loadOp: "clear",
+      clearValue: { r: 0, g: 0, b: 0.4, a: 1 }, // New line
+      storeOp: "store",
+    }]
+  });
+
+  // After encoder.beginRenderPass()
+
+  pass.setPipeline(cellPipeline);
+  pass.setVertexBuffer(0, vertexBuffer);
+  pass.draw(vertices.length / 2); // 6 vertices
+
+  // before pass.end()
+
+  pass.end();
+  const commandBuffer = encoder.finish();
+
+  device.queue.submit([commandBuffer]);
+
+  // Finish the command buffer and immediately submit it.
+  device.queue.submit([encoder.finish()]);
+}
+
+async function appendVoiceChannelName() {
+  const app = document.querySelector('#app');
+
+  let activityChannelName = 'Unknown';
+
+  // Requesting the channel in GDMs (when the guild ID is null) requires
+  // the dm_channels.read scope which requires Discord approval.
+  if (discordSdk.channelId != null && discordSdk.guildId != null) {
+    // Over RPC collect info about the channel
+    const channel = await discordSdk.commands.getChannel({ channel_id: discordSdk.channelId });
+    if (channel.name != null) {
+      activityChannelName = channel.name;
+    }
+  }
+
+  // Update the UI with the name of the current voice channel
+  const textTagString = `Activity Channel: "${activityChannelName}"`;
+  const textTag = document.createElement('p');
+  textTag.textContent = textTagString;
+  app.appendChild(textTag);
+}
+
+async function appendGuildAvatar() {
+  const app = document.querySelector('#app');
+
+  // 1. From the HTTP API fetch a list of all of the user's guilds
+  const guilds = await fetch(`https://discord.com/api/v10/users/@me/guilds`, {
+    headers: {
+      // NOTE: we're using the access_token provided by the "authenticate" command
+      Authorization: `Bearer ${auth.access_token}`,
+      'Content-Type': 'application/json',
+    },
+  }).then((response) => response.json());
+
+  // 2. Find the current guild's info, including it's "icon"
+  const currentGuild = guilds.find((g) => g.id === discordSdk.guildId);
+
+  // 3. Append to the UI an img tag with the related information
+  if (currentGuild != null) {
+    const guildImg = document.createElement('img');
+    guildImg.setAttribute(
+      'src',
+      // More info on image formatting here: https://discord.com/developers/docs/reference#image-formatting
+      `https://cdn.discordapp.com/icons/${currentGuild.id}/${currentGuild.icon}.webp?size=128`
+    );
+    guildImg.setAttribute('width', '128px');
+    guildImg.setAttribute('height', '128px');
+    guildImg.setAttribute('style', 'border-radius: 50%;');
+    app.appendChild(guildImg);
+  }
+}
