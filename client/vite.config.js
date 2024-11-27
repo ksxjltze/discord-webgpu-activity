af89a36ee0a4e58ca68c1c4c0e96b597e5433cfc
Lee Jia Keat <leejiakeat@gmail.com>
2024-11-27 14:02:46 +0800

    test

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,20 +1,25 @@
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  envDir: '../',
-  base: 'dev',
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-        secure: false,
-        ws: true,
+export default defineConfig(({ mode }) => {
+  const isDev = mode === 'development';
+  const isProd = mode === 'production';
+
+  return {
+    envDir: '../',
+    base: isDev ? '/dev' : '/',
+    server: {
+      // proxy: {
+      //   '/api': {
+      //     target: 'http://localhost:3001',
+      //     changeOrigin: true,
+      //     secure: false,
+      //     ws: true,
+      //   },
+      // },
+      hmr: {
+        clientPort: 443,
       },
     },
-    hmr: {
-      clientPort: 443,
-    },
-  },
+  }
 });
