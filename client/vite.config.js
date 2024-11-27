bbbf4546d3a92abbf89f77153081b9e960462396
Lee Jia Keat <leejiakeat@gmail.com>
2024-11-27 16:02:33 +0800

    server url

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,22 +1,27 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
+  process.env = loadEnv(mode, process.cwd());
+
   const isDev = mode === 'development';
   const isProd = mode === 'production';
+
+  const isLocalServer = false;
+  const serverURL = process.env.VITE_SERVER_URL;
 
   return {
     envDir: '../',
     base: isDev ? '/dev' : '/',
     server: {
-      // proxy: {
-      //   '/api': {
-      //     target: 'http://localhost:3001',
-      //     changeOrigin: true,
-      //     secure: false,
-      //     ws: true,
-      //   },
-      // },
+      proxy: {
+        '/api': {
+          target: isLocalServer ? 'http://localhost:3001' : serverURL,
+          changeOrigin: true,
+          secure: false,
+          ws: true,
+        },
+      },
       hmr: {
         clientPort: 443,
       },
