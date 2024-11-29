4f5fc484d6b8ff1453c404ec103d70a442eb75da
Lee Jia Keat <leejiakeat@gmail.com>
2024-11-29 14:29:33 +0800

    dada

---
+++ engine/src/lib.rs
@@ -0,0 +1,98 @@
+use winit::application::ApplicationHandler;
+use winit::event::WindowEvent;
+use winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop};
+use winit::window::{Window, WindowId};
+
+#[cfg(target_arch = "wasm32")]
+use wasm_bindgen::prelude::*;
+
+#[derive(Default)]
+struct App {
+    window: Option<Window>,
+}
+
+impl ApplicationHandler for App {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        self.window = Some(
+            event_loop
+                .create_window(Window::default_attributes())
+                .unwrap(),
+        );
+
+        #[cfg(target_arch = "wasm32")]
+        {
+            match &self.window {
+                Some(w) => {
+                    // Winit prevents sizing with CSS, so we have to set
+                    // the size manually when on web.
+                    use winit::dpi::PhysicalSize;
+                    let _ = w.request_inner_size(PhysicalSize::new(450, 400));
+
+                    use winit::platform::web::WindowExtWebSys;
+                    web_sys::window()
+                        .and_then(|win| win.document())
+                        .and_then(|doc| {
+                            let dst = doc.get_element_by_id("wasm-example")?;
+                            let canvas = web_sys::Element::from(w.canvas()?);
+                            dst.append_child(&canvas).ok()?;
+                            Some(())
+                        })
+                        .expect("Couldn't append canvas to document body.");
+                }
+                _ => println!("Something went wrong!")
+            }
+        }
+    }
+
+    fn window_event(&mut self, event_loop: &ActiveEventLoop, id: WindowId, event: WindowEvent) {
+        match event {
+            WindowEvent::CloseRequested => {
+                println!("The close button was pressed; stopping");
+                event_loop.exit();
+            }
+            WindowEvent::RedrawRequested => {
+                // Redraw the application.
+                //
+                // It's preferable for applications that do not render continuously to render in
+                // this event rather than in AboutToWait, since rendering in here allows
+                // the program to gracefully handle redraws requested by the OS.
+
+                // Draw.
+
+                // Queue a RedrawRequested event.
+                //
+                // You only need to call this if you've determined that you need to redraw in
+                // applications which do not always need to. Applications that redraw continuously
+                // can render here instead.
+                self.window.as_ref().unwrap().request_redraw();
+            }
+            _ => (),
+        }
+    }
+}
+
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen(start))]
+pub fn run() {
+    cfg_if::cfg_if! {
+        if #[cfg(target_arch = "wasm32")] {
+            std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+            console_log::init_with_level(log::Level::Warn).expect("Couldn't initialize logger");
+        } else {
+            env_logger::init();
+        }
+    }
+
+    let event_loop = EventLoop::new().unwrap();
+
+    // ControlFlow::Poll continuously runs the event loop, even if the OS hasn't
+    // dispatched any events. This is ideal for games and similar applications.
+    event_loop.set_control_flow(ControlFlow::Poll);
+
+    // ControlFlow::Wait pauses the event loop if no events are available to process.
+    // This is ideal for non-game applications that only update in response to user
+    // input, and uses significantly less power/CPU time than ControlFlow::Poll.
+    event_loop.set_control_flow(ControlFlow::Wait);
+
+    let mut app = App::default();
+    let _ = event_loop.run_app(&mut app);
+}
---
+++ engine/src/main.rs
@@ -0,0 +1,5 @@
+use wubengine::run;
+
+fn main() {
+    run();
+}
